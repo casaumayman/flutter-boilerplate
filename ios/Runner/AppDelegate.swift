c2647b2c89bb81cebef10c6e74f8b629d296bc3c
casaumayman <casaumayman1@gmail.com>
2022-05-24 14:42:29 +0700

    ok iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,24 +8,47 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
       let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-      let scanNFCChannel = FlutterMethodChannel(name: "scan_NFC",
+      let scanNFCChannel = FlutterMethodChannel(name: "com.example.car_cleaning_demo_flutter.chanel",
                                                 binaryMessenger: controller.binaryMessenger)
       scanNFCChannel.setMethodCallHandler({
         (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-          guard call.method == "getNFCUserInfo" else {
+          switch call.method {
+          case "TEST_COMMUNICATE":
+              if let params = call.arguments as? Dictionary<String, Bool> {
+                  print(params)
+                  let isError = params["isError"] ?? true
+                  testCommunicate(result: result, isError: isError)
+              }
+              result(FlutterError(code: "SCAN_NFC", message: "Invalid format", details: nil))
+          case "SCAN_NFC":
+              if let params = call.arguments as? Dictionary<String, String> {
+                  if #available(iOS 13.0, *) {
+                      let pin1 = params["pin1"] ?? ""
+                      let pin2 = params["pin2"] ?? ""
+                      if (pin1.isEmpty && pin2.isEmpty) {
+                          result(FlutterError(code: "SCAN_NFC", message: "Invalid format", details: nil))
+                          return
+                      }
+                      let scanner = NFCReader(pin1input: pin1, pin2input: pin2, call: call, result: result)
+                      scanner.scanRequest()
+                  } else {
+                      result(FlutterError(code: "SCAN_NFC", message: "iOS 13+ is required", details: nil))
+                  }
+                  return
+              }
+              result(FlutterError(code: "SCAN_NFC", message: "Invalid format", details: nil))
+          default:
               result(FlutterMethodNotImplemented)
-              return
-            }
-          scanNFC(result: result, isError: false)
+          }
       })
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
 
-private func scanNFC(result: FlutterResult, isError: Bool) {
+private func testCommunicate(result: FlutterResult, isError: Bool) {
     if isError == true {
-        result(FlutterError(code: "ERR_CODE_TEST", message: "Message error", details: "Error detail"))
+        result(FlutterError(code: "SCAN_NFC", message: "Message error", details: "Error detail"))
         return
     }
     result("This result from Swift")
