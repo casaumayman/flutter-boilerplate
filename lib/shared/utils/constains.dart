1f7f8edb7adc09664de06fe611a4c84c211a458b
casaumayman <casaumayman1@gmail.com>
2022-05-24 14:43:00 +0700

    ok iOS

--- lib/shared/utils/constains.dart
+++ lib/shared/utils/constains.dart
@@ -1,4 +1,5 @@
 class Constrains{
   static const CHANNEL = "com.example.car_cleaning_demo_flutter.chanel";
-  static const KEY_NATIVE = "showNfc";
+  static const KEY_NATIVE = "SCAN_NFC";
+  static const TEST_COMMUNICATE = "TEST_COMMUNICATE";
 }
